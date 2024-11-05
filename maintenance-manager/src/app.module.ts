2a75fcd04fd884227e0bbac35241743b03a8e574
Carlos Henrique <carlosgfkp@outook.com>
2024-11-05 16:19:04 -0300

    feat: add main module files

---
+++ maintenance-manager/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module, ValidationPipe } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './user/users.module';
+import { AuthModule } from './auth/auth.module';
+import { UserController } from './user/users.controller';
+import { APP_PIPE } from '@nestjs/core';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/maintenance-manager'),
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [UserController],
+  providers: [ {
+    provide: APP_PIPE,
+    useClass: ValidationPipe,
+  }],
+})
+export class AppModule {}
