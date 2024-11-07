c48a68c6a3bf353e05b2ae21a54f22ed3ce5f279
RafaelRodrigues44 <rafael.rodrigues85@hotmail.com>
2024-11-07 20:36:01 -0300

    ajustes na develop

--- maintenance-manager/src/main.ts
+++ maintenance-manager/src/main.ts
@@ -5,6 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3001',  
+    methods: 'GET,POST,PUT,DELETE',  
+    allowedHeaders: 'Content-Type, Authorization',  
+    credentials: true,  
+  });
+
   const config = new DocumentBuilder()
     .setTitle('maintenance-manager API')
     .setDescription('Documentação...')
