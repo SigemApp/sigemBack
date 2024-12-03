fd43b645b0786d375567bb63171642446ed3fae4
Felipe-Camargo12 <felipeca1268@gmail.com>
2024-12-02 23:14:42 -0300

    feat: Correções para liberar o endpoint maintenace para teste

--- maintenance-manager/src/maintenance/maintenance.controller.ts
+++ maintenance-manager/src/maintenance/maintenance.controller.ts
@@ -10,26 +10,22 @@ import { ApiTags } from '@nestjs/swagger';
 @Controller('maintenance')
 export class MaintenanceController {
   constructor(private readonly maintenanceService: MaintenanceService) {}
-
-  @UseGuards(AuthGuard)  
+ 
   @Post()
   async create(@Body() createMaintenanceDto: CreateMaintenanceDto): Promise<Maintenance> {
     return this.maintenanceService.create(createMaintenanceDto);
   }
 
-  @UseGuards(AuthGuard)  
   @Get(':id')
   async findById(@Param('id') id: string): Promise<Maintenance> {
     return this.maintenanceService.findById(id);
   }
 
-  @UseGuards(AuthGuard)  
   @Get()
   async findAll(): Promise<Maintenance[]> {
     return this.maintenanceService.findAll();
   }
 
-  @UseGuards(AuthGuard)  
   @Put(':id')
   async update(
     @Param('id') id: string,
@@ -38,7 +34,6 @@ export class MaintenanceController {
     return this.maintenanceService.update(id, updateMaintenanceDto);
   }
 
-  @UseGuards(AuthGuard)  
   @Delete(':id')
   async delete(@Param('id') id: string): Promise<void> {
     return this.maintenanceService.delete(id);
